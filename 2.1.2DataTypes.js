6b9c4fbb8a412ec4c14976ba54f728654dc549fd
iroj <iroj@fresnoU.org>
2023-02-06 09:50:12 -0800

    Initial Commit

---
+++ 2.1.2DataTypes.js
@@ -0,0 +1,42 @@
+console.log(8);
+//This is a comment
+
+//Numbers
+//Integers
+console.log(-3);
+console.log(0);
+console.log(3);
+
+//Floating Point Number
+console.log(100.045);
+
+//Strings
+//Characters, Letters, symbols, spaces
+console.log("hello world");
+console.log('hello world');
+console.log(`hello world`);
+console.log('16.0');
+console.log("Hello"+"\nWorld!");
+
+//Arithmetic Operators (math)
+console.log(14+2);
+console.log(14-2);
+console.log(14*2);
+console.log(14/12);
+console.log(14%2); //% is called MOD and means remainder after dividing
+console.log(15%10);
+console.log(31%4);
+
+//Concatenation: combine strings
+console.log("My name"+" is Mia");
+console.log('my' + 'name');
+console.log('My' + ' ' + 'age' + ' '+'is'+ '' + 15 + '.');
+console.log("Not"+" "+"today"+".");
+console.log("Your age is\n17."); // \n means new line
+console.log("Your age/n is 14.");
+
+//Template Literal - plug in a calculated value
+//Use backticks `` next to the 1
+console.log(`15 divided by 10 = ${15/10} adfadzxdf`);
+console.log(`12 + 5 = ${12+5}`);
+console.log(`101 divided by 5 has the remainder of: ${101%5}`);
